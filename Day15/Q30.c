67d4295c3c10596af28f844a61b2c8957ddedd78
Ayush Mishra <309825774+atyushmish@users.noreply.github.com>
2026-09-05 10:38:26 +0530

    first commit

---
+++ Day15/Q30.c
@@ -0,0 +1,29 @@
+
+/*
+Q30: Write a program to reverse a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4321
+
+Input 2:
+100
+Output 2:
+1
+
+*/
+#include<stdio.h>
+int main(){
+    int num, reversed = 0;
+    printf("enter the number:");
+    scanf("%d",&num);
+    while(num != 0){
+        reversed = reversed * 10 + num % 10;
+        num /= 10;
+    }
+    printf("reversed number is %d", reversed);
+    return 0;
+}
